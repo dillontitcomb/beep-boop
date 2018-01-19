e3031db4751b8c8fec7b95844bbdeb40426b0333
Dillon Titcomb <dillontitcomb@gmail.com>
2018-01-19 10:56:51 -0800

    Adds logic that successfully returns boop and beep when 1s or z0s are present in the input.

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,36 @@
 var numArray = [0]
 
+var numToString = function(num) {
+	var string = num.toString();
+	alert(string + "hi");
+	if (string.includes("1") === true) {
+		return "boop";
+	} else if (string.includes("0") === true) {
+		return "beep";
+	}
+};
 
-$(document).ready(function() {
-  $("form#numForm").submit(function(event) {
-		var userInput = $("#numInput").val();
-		var total = 0;
-		for (i=0; i<userInput; i++) {
-			total += 1;
-			numArray.push(total);
-		}
-		alert(numArray);
-		$("p.showArray").text(numArray);
-		$("#changedArray").show();
-		event.preventDefault();
-	});
-});
+alert(numToString(50));
+//
+// $(document).ready(function() {
+//   $("form#numForm").submit(function(event) {
+// 		var userInput = $("#numInput").val();
+// 		var total = 0;
+// 		for (i=0; i<userInput; i++) {
+// 			total += 1;
+// 			numArray.push(total);
+// 		}
+// 		alert(numArray);
+// 		var daveArray = numArray.map(function(number) {
+// 			if (number % 3 === 0 && number !== 0) {
+// 				return ("I'm sorry Dave. I'm afraid I can't do that.");
+// 			};
+//
+// 		});
+//
+// 		alert(changedArray);
+// 		$("p.showArray").text(numArray);
+// 		$("#results").show();
+// 		event.preventDefault();
+// 	});
+// });
