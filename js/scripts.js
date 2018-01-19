fc79a6b0dbea4e91b45d64ffe84f923b456d9e46
Dillon Titcomb <dillontitcomb@gmail.com>
2018-01-19 10:14:39 -0800

    Adds hiding and showing for the changed array and some spacing styling.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,17 @@
+var numArray = [0]
+
+
 $(document).ready(function() {
   $("form#numForm").submit(function(event) {
-		alert("hello!");
+		var userInput = $("#numInput").val();
+		var total = 0;
+		for (i=0; i<userInput; i++) {
+			total += 1;
+			numArray.push(total);
+		}
+		alert(numArray);
+		$("p.showArray").text(numArray);
+		$("#changedArray").show();
 		event.preventDefault();
 	});
 });
