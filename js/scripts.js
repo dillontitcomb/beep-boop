879c5d69ec0f30346232e9e964058281bec7c3a4
Dillon Titcomb <dillontitcomb@gmail.com>
2018-01-19 11:13:13 -0800

    Adds if else conditionals that change array elements into beep boop or Dave string and outputs them when button is clicked.

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,33 @@
-var numArray = [0]
-
-var numToString = function(num) {
-	var string = num.toString();
-	alert(string + "hi");
+var numToString = function(number) {
+	var string = number.toString();
 	if (string.includes("1") === true) {
 		return "boop";
 	} else if (string.includes("0") === true) {
 		return "beep";
+	} else {
+		return number;
 	}
 };
 
-alert(numToString(50));
-//
-// $(document).ready(function() {
-//   $("form#numForm").submit(function(event) {
-// 		var userInput = $("#numInput").val();
-// 		var total = 0;
-// 		for (i=0; i<userInput; i++) {
-// 			total += 1;
-// 			numArray.push(total);
-// 		}
-// 		alert(numArray);
-// 		var daveArray = numArray.map(function(number) {
-// 			if (number % 3 === 0 && number !== 0) {
-// 				return ("I'm sorry Dave. I'm afraid I can't do that.");
-// 			};
-//
-// 		});
-//
-// 		alert(changedArray);
-// 		$("p.showArray").text(numArray);
-// 		$("#results").show();
-// 		event.preventDefault();
-// 	});
-// });
+$(document).ready(function() {
+  $("form#numForm").submit(function(event) {
+		var numArray = []
+		var changedArray = [];
+		var userInput = $("#numInput").val();
+		var total = 0;
+		for (i=0; i<userInput; i++) {
+			total += 1;
+			numArray.push(total);
+		}
+		numArray.forEach(function(number) {
+			if (number % 3 === 0 && number !== 0) {
+					changedArray.push("I'm sorry Dave. I'm afraid I can't do that.");
+			} else {
+					changedArray.push(numToString(number));
+			}
+		});
+		$("p.showArray").text(changedArray);
+		$("#results").show();
+		event.preventDefault();
+	});
+});
